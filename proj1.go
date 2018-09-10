487e186907e4a5ce45e0108d02f6be1e4ee64070
zachglee <zacharygowlee@gmail.com>
2018-09-10 16:01:21 -0400

    ok i think i finally figured out what the issue is

--- proj1.go
+++ proj1.go
@@ -8,19 +8,23 @@ import (
 )
 
 func main() {
-  var port string = "27993"
-  //var port string = "80"
+  //var port string = "27993"
+  //var port string = "80" //Does string addition with '+' really work in go?
+
+  //var host string = "login-faculty.ccs.neu.edu"
+  //var helloMsg string = "cs3700fall2018 HELLO 001698478\n"
+  //var testMsg string = "HEAD / HTTP/1.0\r\n\r\n"
   if (len(os.Args) == 5 && os.Args[1] == "-p") {
-    port = os.Args[2]
+    //port = os.Args[2]
   }
-  tcpAddr, err := net.ResolveTCPAddr("tcp4", "cbw.sh:" + port)
+  tcpAddr, err := net.ResolveTCPAddr("tcp4", "cbw.sh:27993")
   checkError(err)
+  fmt.Println("got tcpAddr")
   conn, err := net.DialTCP("tcp", nil, tcpAddr)
   checkError(err)
-  fmt.Println("right before printing conn")
   fmt.Println(conn)
 
-  _, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+  _, err = conn.Write([]byte("cs3700fall2018 HELLO 001698478\n"))
   checkError(err)
   result, err := ioutil.ReadAll(conn)
   fmt.Println(result)
