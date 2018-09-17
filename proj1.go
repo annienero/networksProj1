f872cdeba426898008193c7bc31022ee5cd5bd6f
Annie <annienero93@gmail.com>
2018-09-17 11:48:55 -0400

    first find working, second doens't read full message

--- proj1.go
+++ proj1.go
@@ -4,14 +4,15 @@ import (
     "net"
     "fmt"
     "os"
-    "time"
     "strings"
     "strconv"
 )
 
 func main() {
-  var port string = "27993"
-  var helloMsg string = "cs3700fall2018 HELLO 001698478\n"
+  port := "27993"
+  helloMsg := "cs3700fall2018 HELLO 001698478\n"
+  countMsg := "cs3700fall2018 COUNT "
+
 
   if (len(os.Args) == 5 && os.Args[1] == "-p") {
     port = os.Args[2]
@@ -21,22 +22,28 @@ func main() {
   checkError(err)
   conn, err := net.DialTCP("tcp", nil, tcpAddr)
   checkError(err)
-  conn.SetReadDeadline(time.Now().Add(time.Second))
   _, err = conn.Write([]byte(helloMsg))
   checkError(err)
 
-  var tmp = make([]byte, 256)
-  _, err = conn.Read(tmp)
-  checkError(err)
-  var message = ""
-  for tmp[0] != 0 {
-    var str = string(tmp[:256])
-    message += str
-    tmp = make([]byte, 256)
-    conn.Read(tmp)
+  tokens := strings.Split(readFromConnection(conn), " ")
+  messageType := tokens[1]
+  arguments := tokens[2:len(tokens)]
+
+  for messageType != "BYE" {
+    count := countMsg + strconv.Itoa(evalFind(arguments)) + "\n"
+    fmt.Println("countMessage = " + count)
+    _, err = conn.Write([]byte(count))
+    checkError(err)
+
+    tokens = strings.Split(readFromConnection(conn), " ")
+    messageType = tokens[1]
+    arguments = tokens[2:len(tokens)]
   }
-  _, err = conn.Write([]byte(executeAndGetReply(message)))
-  checkError(err)
+
+  // print secret flag
+  fmt.Println(arguments[0])
+
+  conn.Close()
 }
 
 func checkError(err error) {
@@ -46,19 +53,22 @@ func checkError(err error) {
   }
 }
 
-//A message is a one of the 4 possible messages in this protocol, as described by the assignment
-//A message's type is one of: [HELLO, FIND, COUNT, BYE]
-func executeAndGetReply(messageString string) string {
-
-  tokens := strings.Split(messageString, " ")
-  messageType := tokens[1]
-  arguments := tokens[2:len(tokens)]
-
-  if messageType == "FIND" {
-    return "cs3700fall2018 COUNT " + strconv.Itoa(evalFind(arguments)) + "\n"
-  } else {  
-    return "Bye case not implemented yet."
+func readFromConnection(conn *net.TCPConn) string {
+  tmp := make([]byte, 256)
+  bytes_read, err := conn.Read(tmp)
+  checkError(err)
+  message := ""
+  for bytes_read == 256 {
+    str := string(tmp[:256])
+    message += str
+    tmp = make([]byte, 256)
+    bytes_read, err = conn.Read(tmp)
+    checkError(err)
   }
+  fmt.Println("bytes read " + strconv.Itoa(bytes_read))
+  message += string(tmp[:256])
+  fmt.Println("full message is " + message)
+  return message
 }
 
 func evalFind(arguments []string) int {
